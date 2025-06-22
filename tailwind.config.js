47ee1e5b37e0fd1405c309efe7052a8213e9ebfb
esanedo <esanedo@gmail.com>
2025-06-22 21:36:55 +0000

    Second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
-module.exports = {
+/*module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
+    extend: {},
+  },
+  plugins: [],
+} 
+  
+*/
+//
+//export default {
+//  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [],
+//}
+
+// tailwind.config.js
+export default {
+  content: ['./frontend/index.html', './frontend/src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
     extend: {},
   },
   plugins: [],
